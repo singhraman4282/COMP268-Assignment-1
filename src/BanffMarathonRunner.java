2b8069f3c3f5380ab177508f531505cf616dd75a
Raman Singh <ramans@kater.com>
2019-07-04 19:48:10 -0700

    3 done

---
+++ src/BanffMarathonRunner.java
@@ -0,0 +1,85 @@
+public class BanffMarathonRunner extends AddressBook {
+
+    private int time;
+    private int years;
+
+
+    public BanffMarathonRunner(String fn, String ln, int min, int yr) {
+        super(fn, "", ln);
+        this.time = min;
+        this.years = yr;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public static BanffMarathonRunner getFastestRunner(BanffMarathonRunner[] runners) {
+        BanffMarathonRunner fastestRunner = runners[0];
+
+        for (int i=0; i<runners.length; i++) {
+            if (runners[i].getTime() < fastestRunner.getTime())
+                fastestRunner = runners[i];
+        }
+
+        return fastestRunner;
+    }
+
+    public static BanffMarathonRunner getSecondFastestRunner(BanffMarathonRunner[] runners) {
+        BanffMarathonRunner fastestRunner = getFastestRunner(runners);
+        BanffMarathonRunner secondFastestRunner = runners[0];
+        if (secondFastestRunner.getTime() == fastestRunner.getTime()) {
+            secondFastestRunner = runners[1];
+        }
+
+        for (int i=0; i<runners.length; i++) {
+            if (runners[i].getTime() > fastestRunner.getTime()) {
+                if (runners[i].getTime() < secondFastestRunner.getTime()) {
+                    secondFastestRunner = runners[i];
+                }
+            }
+        }
+
+        return secondFastestRunner;
+    }
+
+    public static double getAverageTime(BanffMarathonRunner[] runners) {
+        double totalTime = 0;
+        for (int i=0; i<runners.length; i++) {
+            totalTime += runners[i].getTime();
+        }
+
+        return totalTime/runners.length;
+    }
+
+    public static String getAboveAverageRunners(BanffMarathonRunner[] runners) {
+        double averageTime = getAverageTime(runners);
+        String details = "";
+        for (int i=0; i<runners.length; i++) {
+            if (runners[i].getTime() <= averageTime) {
+                String runnerDetails = runners[i].getFirstName() + " " + runners[i].getMiddleName() + " " + runners[i].getLastName() + ", " + runners[i].getYears();
+                if (details.length() == 0) {
+                    details = runnerDetails;
+                } else {
+                    details = details + "\n" + runnerDetails;
+                }
+            }
+        }
+
+        return details;
+    }
+
+
+}
---
+++ src/Question4.java
@@ -0,0 +1,71 @@
+/*
+A group of AU friends decide to run the Banff, Alberta, Marathon. Their names, times (marathon
+completion time in minutes), and number of years participated are given below:
+1 Elena Brandon 341 1
+2 Thomas Molson 273 2
+3 Hamilton Winn 278 5
+4 Suzie Sarandin 329 7
+5 Philip Winne 445 9
+6 Alex Trebok 275 3
+7 Emma Pivoto 275 4
+8 John Lenthen 243 1
+9 James Lean 334 1
+10 Jane Ostin 412 1
+11 Emily Car 393 4
+12 Daniel Hamshire 299 4
+13 Neda Bazdar 343 3
+14 Aaron Smith 317 6
+15 Kate Hen 265 8
+
+Extend the AddressBook class from Problem 1 to store the additional data. Now, write a method
+to find the fastest runner. Print the name, address, and his/her time (in minutes) on three separate
+lines.
+Find the second fastest runner. Print the name, address, his/her time (in minutes), and the
+difference in time with the fastest runner.
+Compute the average time of completion taken by these runners.
+Finally, print the name and number of years participated for each runner if the runner’s time of
+completion is equal to or better than the average time of completion.
+ */
+
+public class Question4 {
+
+    public static void main(String[] args) {
+        BanffMarathonRunner[] runners = new BanffMarathonRunner[] {
+                new BanffMarathonRunner("Elena", "Brandon", 341, 1),
+                new BanffMarathonRunner("Thomas", "Molson", 273, 2),
+                new BanffMarathonRunner("Hamilton", "Winn", 278, 5),
+                new BanffMarathonRunner("Suzie", "Sarandin", 329, 7),
+                new BanffMarathonRunner("Philip", "Winne", 445, 9),
+                new BanffMarathonRunner("Alex", "Trebok", 275, 3),
+                new BanffMarathonRunner("Emma", "Pivoto", 275, 4),
+                new BanffMarathonRunner("John", "Lenthen", 243, 1),
+                new BanffMarathonRunner("James", "Lean", 334, 1),
+                new BanffMarathonRunner("Jane", "Ostin", 412, 1),
+                new BanffMarathonRunner("Emily", "Car", 393, 4),
+                new BanffMarathonRunner("Daniel", "Hamshire", 299, 4),
+                new BanffMarathonRunner("Neda", "Bazdar", 343, 3),
+                new BanffMarathonRunner("Aaron", "Smith", 317, 6),
+                new BanffMarathonRunner("Kate", "Hen", 265, 8) };
+
+        for (int i=0; i<runners.length; i++) {
+            runners[i].setHomeAddress("345 6 Ave SE 6th Floor, Calgary, AB T2G 4V1");
+        }
+
+        BanffMarathonRunner fastestRunner = BanffMarathonRunner.getFastestRunner(runners);
+        System.out.println(fastestRunner.getFirstName() + " " + fastestRunner.getLastName());
+        System.out.println(fastestRunner.getHomeAddress());
+        System.out.println(fastestRunner.getTime());
+
+        BanffMarathonRunner secondRunner = BanffMarathonRunner.getSecondFastestRunner(runners);
+        System.out.println(secondRunner.getFirstName() + " " + secondRunner.getLastName());
+        System.out.println(secondRunner.getHomeAddress());
+        System.out.println(secondRunner.getTime());
+        System.out.println("Time diffference: " + (secondRunner.getTime() - fastestRunner.getTime()));
+
+        double averageTime = BanffMarathonRunner.getAverageTime(runners);
+        System.out.println("Average time: " + averageTime);
+
+        String aboveAveragePlayers = BanffMarathonRunner.getAboveAverageRunners(runners);
+        System.out.println(aboveAveragePlayers);
+    }
+}
